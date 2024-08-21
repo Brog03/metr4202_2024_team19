604f7eb1ab91a6c62d7816fc0610334c65d0f25e
Jack Boyd <s4750003@students.uq.edu.au>
2024-08-21 12:49:42 +1000

    hihefr

--- repo_setup.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-current_dir=$(pwd)
-echo "source ${current_dir}/metr4202_ws/install/setup.bash" >> ~/.bashrc
-echo "export PATH=${current_dir}/bin:\$PATH" >> ~/.bashrc
-source ~/.bashrc
