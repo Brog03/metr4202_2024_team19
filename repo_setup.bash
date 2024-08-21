de0813b1ecf727f6b7e1f74fc4ccbca093414ccd
Jack Boyd <s4750003@students.uq.edu.au>
2024-08-21 12:36:22 +1000

    WORK

--- repo_setup.bash
+++ repo_setup.bash
@@ -2,5 +2,5 @@
 
 current_dir=$(pwd)
 echo "source ${current_dir}/metr4202_ws/install/setup.bash" >> ~/.bashrc
-echo "export PATH=$current_dir/bin:$PATH" >> ~/.bashrc
+echo "export PATH=${current_dir}/bin:\$PATH" >> ~/.bashrc
 source ~/.bashrc
